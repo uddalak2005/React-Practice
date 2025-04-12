07adb62794df12de1a27692bd64b34f05da7800a
uddalak2005 <uddalakmukhopadhyay@gmail.com>
2025-04-12 16:40:51 +0530

    Revising React Day 1

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -1,16 +1,29 @@
+//importing dependencies
 import { react } from "react";
+import { useState } from "react";
 //import css
-import './App.css'
+import "./App.css";
+import styles from './components/Button.module.css'
 //import components
 import Button from "./components/Button";
-import List from './components/List'
-
+import List from "./components/List";
 
 const user = {
   name: "Hedy Lamarr",
   imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
   imageSize: 90,
 };
+
+function CountButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick(event) {
+    event.preventDefault();
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick} className={styles.btn}>Clicked {count} times !</button>;
+}
 
 function App() {
   return (
@@ -19,7 +32,7 @@ function App() {
         <img
           src={user.imageUrl}
           alt={`Photo of ${user.name}`}
-          className = "avatar"
+          className="avatar"
           style={{
             width: user.imageSize,
             height: user.imageSize,
@@ -29,9 +42,12 @@ function App() {
       </div>
 
       <Button />
+
       <div>
         <List />
       </div>
+
+      <CountButton />
     </div>
   );
 }
